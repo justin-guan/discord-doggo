e4327df46840a41ad97b54cd950d8b5c448d5b42
justin-guan <dengitzjg@gmail.com>
2018-11-07 00:16:15 -0500

    Add store tests

--- test/store/store.spec.ts
+++ test/store/store.spec.ts
@@ -1,26 +1,46 @@
+// ignore max classes per file to allow for creating mock implementations
+// tslint:disable:max-classes-per-file
 const mockDatabaseInitialize = jest.fn();
 const mockDatabaseClose = jest.fn();
+const mockGetGuild = jest.fn();
 jest.mock("@store/mongo/database-store", () => {
   return class {
     public connect = mockDatabaseInitialize;
     public close = mockDatabaseClose;
+    public getGuild = mockGetGuild;
   };
 });
-jest.mock("@store/commands/command-store");
+const mockGetCommand = jest.fn();
+jest.mock("@store/commands/command-store", () => {
+  return class {
+    public getCommand = mockGetCommand;
+  };
+});
 
+import Command from "@store/commands/command";
+import Guild from "@store/models/guild";
 import Store from "@store/store";
+import * as TypeMoq from "typemoq";
 
 describe("Store", () => {
   let store: Store;
   const testDatabaseUri = "test database uri";
+  const testCommandPrefix = "!";
+  const testServerId = "test server id";
+  const testCommandName = "test command name";
+  const mockGuild = TypeMoq.Mock.ofType<Guild>();
 
   beforeEach(() => {
     store = new Store();
+    setupMockGuild();
     mockDatabaseInitialize.mockImplementation(() => {
       return Promise.resolve();
     });
     mockDatabaseClose.mockImplementation(() => {
       return Promise.resolve();
+    });
+    mockGetGuild.mockImplementation(() => {
+      return Promise.resolve(mockGuild.object);
     });
   });
 
@@ -68,8 +88,114 @@ describe("Store", () => {
     expect(mockDatabaseClose).toBeCalledTimes(1);
   });
 
+  test("should update the command prefix", async () => {
+    mockGuild
+      .setup(g => g.save())
+      .returns(() => Promise.resolve(mockGuild.object));
+    const result = store.updateCommandPrefix({
+      newPrefix: testCommandPrefix,
+      serverId: testServerId
+    });
+
+    await expect(result).resolves.toBeUndefined();
+    mockGuild.verify(
+      g => g.setCommandPrefix(testCommandPrefix),
+      TypeMoq.Times.once()
+    );
+    mockGuild.verify(g => g.save(), TypeMoq.Times.once());
+  });
+
+  test("should fail to get the guild to update the command prefix", async () => {
+    const testError = new Error();
+    mockGetGuild.mockImplementation(() => {
+      return Promise.reject(testError);
+    });
+
+    const result = store.updateCommandPrefix({
+      newPrefix: testCommandPrefix,
+      serverId: testServerId
+    });
+
+    await expect(result).rejects.toBe(testError);
+    mockGuild.verify(
+      g => g.setCommandPrefix(TypeMoq.It.isAnyString()),
+      TypeMoq.Times.never()
+    );
+    mockGuild.verify(g => g.save(), TypeMoq.Times.never());
+  });
+
+  test("should fail to save the command prefix", async () => {
+    const testError = new Error();
+    mockGuild.setup(g => g.save()).returns(() => Promise.reject(testError));
+
+    const result = store.updateCommandPrefix({
+      newPrefix: testCommandPrefix,
+      serverId: testServerId
+    });
+
+    await expect(result).rejects.toBe(testError);
+    mockGuild.verify(
+      g => g.setCommandPrefix(testCommandPrefix),
+      TypeMoq.Times.once()
+    );
+    mockGuild.verify(g => g.save(), TypeMoq.Times.once());
+  });
+
+  test("should get command prefix for a guild", async () => {
+    mockGuild.setup(g => g.getCommandPrefix()).returns(() => testCommandPrefix);
+
+    const result = store.getCommandPrefix(testServerId);
+
+    await expect(result).resolves.toEqual(testCommandPrefix);
+    expect(mockGetGuild).toBeCalledTimes(1);
+    expect(mockGetGuild).toBeCalledWith(testServerId);
+    mockGuild.verify(g => g.getCommandPrefix(), TypeMoq.Times.once());
+  });
+
+  test("should fail to get the guild when trying to get command prefix", async () => {
+    const testError = new Error();
+    mockGetGuild.mockImplementation(() => {
+      return Promise.reject(testError);
+    });
+
+    const result = store.getCommandPrefix(testServerId);
+
+    await expect(result).rejects.toBe(testError);
+    expect(mockGetGuild).toBeCalledTimes(1);
+    expect(mockGetGuild).toBeCalledWith(testServerId);
+    mockGuild.verify(g => g.getCommandPrefix(), TypeMoq.Times.never());
+  });
+
+  test("should find a command given a command name that exists", async () => {
+    mockGetCommand.mockImplementation(() => {
+      return TypeMoq.Mock.ofType<Command>().object;
+    });
+
+    const command = store.getCommand(testServerId, testCommandName);
+
+    expect(command).not.toBeUndefined();
+  });
+
+  test("should not find a command given a command name that exists", async () => {
+    mockGetCommand.mockImplementation(() => {
+      return undefined;
+    });
+
+    const command = store.getCommand(testServerId, testCommandName);
+
+    expect(command).toBeUndefined();
+  });
+
+  function setupMockGuild(): void {
+    // ok to ignore any rule because of mock limitations
+    // tslint:disable-next-line:no-any
+    mockGuild.setup((p: any) => p.then).returns(() => undefined);
+  }
+
   function resetMocks(): void {
     mockDatabaseInitialize.mockReset();
     mockDatabaseClose.mockReset();
+    mockGetGuild.mockReset();
+    mockGuild.reset();
   }
 });
