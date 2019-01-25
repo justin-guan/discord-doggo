5fea404add16dd67249a51928f3ecd8c911967e4
justin-guan <dengitzjg@gmail.com>
2019-01-24 22:49:31 -0500

    Update tests to use a mock implementation of Collection

--- test/model/discord/discord-client-impl.spec.ts
+++ test/model/discord/discord-client-impl.spec.ts
@@ -1,9 +1,44 @@
+jest.mock("discord.js", () => {
+  return {
+    VoiceChannel: class {
+      public join = jest.fn();
+    },
+    // tslint:disable-next-line:max-classes-per-file
+    Collection: class C<K, V> extends Map<K, V> {
+      public some(predicate: (vc: V) => boolean): boolean {
+        for (const entry of this.entries()) {
+          if (predicate(entry[1])) {
+            return true;
+          }
+        }
+        return false;
+      }
+
+      public map<T>(predicate: (vc: V) => T): T[] {
+        const mapped: T[] = [];
+        for (const entry of this.entries()) {
+          mapped.push(predicate(entry[1]));
+        }
+        return mapped;
+      }
+
+      public find(predicate: (vc: V) => boolean): V {
+        for (const entry of this.entries()) {
+          if (predicate(entry[1])) {
+            return entry[1];
+          }
+        }
+        return (null as unknown) as V;
+      }
+    }
+  };
+});
+
 import DiscordClientImpl from "@model/discord/discord-client-impl";
 import {
   Client,
   ClientUser,
   Collection,
-  StreamDispatcher,
   VoiceChannel,
   VoiceConnection
 } from "discord.js";
@@ -12,7 +47,7 @@ import * as TypeMoq from "typemoq";
 describe("Discord Client Implementation", () => {
   test("should get id from discord client", () => {
     const testClientId = "test client id";
-    const mockClient = createMockClient(testClientId, true).object;
+    const mockClient = createMockClient(testClientId, []).object;
     const clientImpl = new DiscordClientImpl(mockClient);
 
     expect(clientImpl.id).toEqual(testClientId);
@@ -20,7 +55,7 @@ describe("Discord Client Implementation", () => {
 
   test("should be in voice channel", () => {
     const voiceId = "voice id";
-    const mockClient = createMockClient("", true);
+    const mockClient = createMockClient("", [voiceId]);
     const clientImpl = new DiscordClientImpl(mockClient.object);
 
     expect(clientImpl.isInVoiceChannel(voiceId)).toEqual(true);
@@ -28,10 +63,33 @@ describe("Discord Client Implementation", () => {
 
   test("should not be in voice channel", () => {
     const voiceId = "voice id";
-    const mockClient = createMockClient("", false);
+    const mockClient = createMockClient("", []);
     const clientImpl = new DiscordClientImpl(mockClient.object);
 
     expect(clientImpl.isInVoiceChannel(voiceId)).toEqual(false);
+  });
+
+  test("should get all voice channels that the client is in", () => {
+    const voiceId1 = "voice id 1";
+    const voiceId2 = "voice id 2";
+    const voiceIds = [voiceId1, voiceId2];
+    const mockClient = createMockClient("", voiceIds);
+    const clientImpl = new DiscordClientImpl(mockClient.object);
+
+    const voiceChannelIds = clientImpl.getConnectedVoiceChannelIds();
+
+    expect(voiceChannelIds).toHaveLength(voiceIds.length);
+    expect(voiceChannelIds[0]).toEqual(voiceId1);
+    expect(voiceChannelIds[1]).toEqual(voiceId2);
+  });
+
+  test("should get no voice channels because the client is not in any", () => {
+    const mockClient = createMockClient("", []);
+    const clientImpl = new DiscordClientImpl(mockClient.object);
+
+    const voiceChannelIds = clientImpl.getConnectedVoiceChannelIds();
+
+    expect(voiceChannelIds).toHaveLength(0);
   });
 
   function createMockUser(userId: string): TypeMoq.IMock<ClientUser> {
@@ -40,29 +98,35 @@ describe("Discord Client Implementation", () => {
     return mock;
   }
 
-  function createMockVoiceConnectionsCollection(
-    voiceConnections: boolean
-  ): TypeMoq.IMock<Collection<string, VoiceConnection>> {
-    const mockCollection = TypeMoq.Mock.ofType<
-      Collection<string, VoiceConnection>
-    >();
-    mockCollection
-      .setup(c => c.some(TypeMoq.It.isAny()))
-      .returns(() => voiceConnections);
-    return mockCollection;
+  function createMockVoiceConnection(
+    id: string
+  ): TypeMoq.IMock<VoiceConnection> {
+    const mock = TypeMoq.Mock.ofType<VoiceConnection>();
+    mock
+      .setup(vc => vc.channel)
+      .returns(() => createMockVoiceChannel(id).object);
+    return mock;
+  }
+
+  function createMockVoiceChannel(id: string): TypeMoq.IMock<VoiceChannel> {
+    const mock = TypeMoq.Mock.ofType<VoiceChannel>();
+    mock.setup(vc => vc.id).returns(() => id);
+    return mock;
   }
 
   function createMockClient(
     userId: string,
-    voiceConnections: boolean
+    voiceConnectionGuildId: string[]
   ): TypeMoq.IMock<Client> {
     const mock = TypeMoq.Mock.ofType<Client>();
     mock.setup(c => c.user).returns(() => createMockUser(userId).object);
-    mock
-      .setup(c => c.voiceConnections)
-      .returns(
-        () => createMockVoiceConnectionsCollection(voiceConnections).object
-      );
+    mock.setup(c => c.voiceConnections).returns(() => {
+      const collection = new Collection<string, VoiceConnection>();
+      voiceConnectionGuildId.forEach((id, idx) => {
+        collection.set(idx.toString(), createMockVoiceConnection(id).object);
+      });
+      return collection;
+    });
     return mock;
   }
 });
